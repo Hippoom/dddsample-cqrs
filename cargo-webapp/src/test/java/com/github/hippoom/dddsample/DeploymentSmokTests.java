6a16181608bf2112fb5e0a8f31e6e7c8836dd776
Hippoom <hippoom@gmail.com>
2013-10-29 22:13:54 +0800

    Skeleton done.

---
+++ cargo-webapp/src/test/java/com/github/hippoom/dddsample/DeploymentSmokTests.java
@@ -0,0 +1,25 @@
+package com.github.hippoom.dddsample;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Request;
+import org.junit.Test;
+
+public class DeploymentSmokTests {
+
+	@Test
+	public void showsWelcomeGivenDeploymentDone() throws Throwable {
+		assertThat(getStatusCodeFromIndexPage(), equalTo(200));
+	}
+
+	private int getStatusCodeFromIndexPage() throws IOException,
+			ClientProtocolException {
+		return Request.Get("http://localhost:9999/cargo/index.jsp").execute()
+				.returnResponse().getStatusLine().getStatusCode();
+
+	}
+}
